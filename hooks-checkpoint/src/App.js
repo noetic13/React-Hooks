eed049a78f35a2e97d6f51dcd9b221f131b516e0
Moemen <moemensaied13@gmail.com>
2023-05-07 11:36:45 +0100

    first commit

---
+++ hooks-checkpoint/src/App.js
@@ -0,0 +1,60 @@
+
+import {useState} from 'react';
+import Movies from './Movies'
+
+function App() {
+  const [movies,setMovies]=useState([{
+    title: 'The Shawshank Redemption',
+    description:'Andy Dufresne, a successful banker, is arrested for the murders of his wife and her lover, and is sentenced to life imprisonment at the Shawshank prison. He becomes the most unconventional prisoner.',
+    posterURL: "https://cdn.shopify.com/s/files/1/0037/8008/3782/products/shawshank_redemption_R04_NG06274_B-440492_f9abf092-fa60-4d3c-954b-a71c67d7b206-912429.jpg",
+    rating: 5
+  },
+  {
+    title: "The Godfather",
+    description: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+    posterURL: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+    rating: 5
+    },
+  {
+    title: "12 Angry Men",
+    description: "A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence.",
+    posterURL: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/e6520b78339627.5ca24af435882.png",
+    rating: 4.9
+  },
+  {
+    title: "Inception",
+    description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+    posterURL: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg",
+    rating: 4.8
+  },
+  {
+    title: "The Lord of the Rings: The Return of the King",
+    description: "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.",
+    posterURL: "https://m.media-amazon.com/images/I/81EBp0vOZZL.jpg",
+    rating: 4.9
+  },
+  {
+    title: "The Dark Knight",
+    description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    posterURL: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
+    rating: 4.7
+  },
+  ])
+
+  
+
+const addMovie=(movie)=>{
+  setMovies(movies.concat(movie))
+}
+
+
+  return (
+    <div>
+
+        <Movies addMovie={addMovie} movies= {movies}/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ hooks-checkpoint/src/Movies.js
@@ -0,0 +1,60 @@
+import './App.css';
+import {useState} from 'react';
+import Add from './Add' ;
+const MovieList = ({movies,addMovie}) => {
+  const [searchTerm, setSearchTerm]=useState('');
+  
+
+  
+
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value.toLowerCase())
+  };
+  
+  const filteredMovies = movies.filter((movies)=>movies.title.toLowerCase().includes(searchTerm));
+
+
+  return (
+    <div>
+
+      
+      <div className="search-container">
+        <input type="text" placeholder="Search..." onChange={handleSearch}></input>
+        
+        <Add addMovie={addMovie}/>
+        
+
+        
+    </div>
+        {filteredMovies.map(movies =>
+      <figure className="movie" style={{margin:"100px"}}>
+        <div className="movie__hero">
+          <img src={movies.posterURL} alt="Movie Poster" className="movie__img" />
+        </div>
+        <div className="movie__content">
+          <div className="movie__title">
+            <h1 className="heading__primary">{movies.title}<i className="fas fa-fire" /></h1>
+            
+          </div>
+          <p className="movie__description">
+            {movies.description}
+          </p>
+          <div className="movie__details">
+            <p className="movie__detail"><span className="icons icons-red"><i className="fas fa-camera-retro" /> </span>Buzz
+              Feitshans</p>
+            <p className="movie__detail"><span className="icons icons-grey"><i className="fas fa-clock" /> </span>1h 33m</p>
+            <p className="movie__detail"><span className="icons icons-yellow"><i className="fas fa-file-invoice-dollar" />
+              </span>Rating : {movies.rating}</p>
+          </div>
+        </div>
+        <div className="movie__price">$12.56</div>
+      </figure>)}
+
+    </div>
+    
+  );
+};
+
+export default MovieList;
+
